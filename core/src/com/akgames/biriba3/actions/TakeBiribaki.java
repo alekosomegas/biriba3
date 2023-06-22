e40b17aa8027cb93a4bc1fd14488ae068ef8889d
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-06-22 12:23:01 +0300

    test folders issue. Biribaki, valid triti joker

---
+++ core/src/com/akgames/biriba3/actions/TakeBiribaki.java
@@ -0,0 +1,31 @@
+package com.akgames.biriba3.actions;
+
+import com.akgames.biriba3.controller.GameLogic;
+import com.akgames.biriba3.model.Card;
+import com.akgames.biriba3.model.Player;
+
+import java.util.List;
+
+public class TakeBiribaki implements PlayerAction {
+
+    @Override
+    public void execute() {
+        List<Card> biribaki = GameLogic.getInstance().getBoard().getBiribaki();
+        GameLogic.getInstance().getCurrentPlayer().addToHand(biribaki);
+        if(GameLogic.getInstance().getPlayers().indexOf(GameLogic.getInstance().getCurrentPlayer()) == 0) {
+            for (Card card : biribaki) {
+                card.turn();
+            }
+        }
+    }
+
+    @Override
+    public void execute(List<?> params) {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
+}
